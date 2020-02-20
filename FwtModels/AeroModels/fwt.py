e0fc0e8255bd555ab8a76ad15a6ad42dde58535c
Fintan Healy <fintanhealy@live.co.uk>
2020-02-20 17:03:21 +0000

    fixed issues with dAlpha prediction, and put in method to extract DAlpha post run

--- FwtModels/AeroModels/fwt.py
+++ FwtModels/AeroModels/fwt.py
@@ -8,7 +8,7 @@ class SimpleAeroModel:
         ## force per unit length will follow theredosons unsteady theory
 
         # symbol for y of FWT
-        y_f = sym.Symbol('y_f')
+        y_t = sym.Symbol('y_t')
 
         # aero focre equation
         _alpha_r, V_t = me.dynamicsymbols('alpha_r V_t')
@@ -22,16 +22,15 @@ class SimpleAeroModel:
         sLambda = sym.sin(p.Lambda)
         n = _alpha_r*ctheta+sLambda*stheta
         d = _alpha_r*sLambda*stheta-sLambda**2*ctheta+sLambda**2-1
-        self.dAlpha = sym.atan(n/d)
+        self.dAlpha = sym.atan(-n/d)
 
-        self.dAlpha = self.dAlpha - ((p.s*half*qd[0]+qd[0]*y_f))/V_t
+        self.dAlpha = self.dAlpha - qd[0]*y_t/V_t
         self.dAlpha = self.dAlpha - (qd[1]*sym.sin(q[0]))/V_t
-        #self.dAlpha = self.dAlpha + _alpha_r*sym.cos(q[0]-sym.pi*half)
         self.dL_w = half*p.rho*V_t**2*p.c*p.a_t*self.dAlpha
 
         #self.dL_w = self.dL_w*sym.cos(self.dAlpha.subs(y_f,0))
         #jacobian per unit length of FWT
-        dr_idq_j= Transform.Transform_point([0,y_f,0]).jacobian(q)
+        dr_idq_j= Transform.Transform_point([0,y_t,0]).jacobian(q)
 
         # force Rotated into task space
         self.dL_wi = Transform.T[:3,:3]*sym.Matrix([0,0,self.dL_w])
@@ -40,11 +39,12 @@ class SimpleAeroModel:
         self.dQ = dr_idq_j.T*self.dL_wi
 
         # generalised force
-        self.Q = self.dQ.integrate((y_f,p.s*-half,p.s*half))
+        self.Q = self.dQ.integrate((y_t,0,p.s))
 
         #convert into a Lambda function
         tup = FwtParams.GetTuple()
         self.func = sym.lambdify((*tup,_alpha_r, V_t,q,qd),self.Q)
+        self.dAlpha_func = sym.lambdify((*tup,_alpha_r, V_t,y_t,q,qd),self.dAlpha)
 
         self._v_func = lambda t: 20
         self._ar_func = lambda t: 0
@@ -53,8 +53,12 @@ class SimpleAeroModel:
         self._v_func = V_Fuc
         self._ar_func = a_r_func
 
+    def GetAlpha(self,FwtParams,q,qd,y_t,t):
+        tup = FwtParams.GetNumericTuple()
+        return self.dAlpha_func(*tup,self._ar_func(t),self._v_func(t),y_t,q,qd)
 
-    def Calc(self,FwtParams,q,qd,t,v_0=20):
+
+    def Calc(self,FwtParams,q,qd,t):
         tup = FwtParams.GetNumericTuple()
         return self.func(*tup,self._ar_func(t),self._v_func(t),q,qd)
 
